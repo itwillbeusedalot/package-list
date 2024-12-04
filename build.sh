6c4cf215befc7a8af3d893446afe91c01fdc60e9
itwillbeusedalot <86040413+itwillbeusedalot@users.noreply.github.com>
2024-12-04 12:37:32 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,9 +2,9 @@ uname -a
 
 cat /etc/*ease
 
-dpkg-query -W --showformat='${Installed-Size}\t${Package}\n' | awk '{printf "%10.2f MB \t%s\n", $1/1024, $2}' | sort -n > package_list_size_in_bytes.txt
+dpkg-query -W --showformat='${Installed-Size}\t${Package}\n' | awk '{printf "%10.2f MB \t%s\n", $1/1024, $2}' | sort -n > package_list_size_in_MB.txt
 
-dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n > package_list.txt
+dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n > package_list_size_in_bytes.txt
 
 
 df -h
