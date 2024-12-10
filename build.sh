b57251289137d32e07c015f22ce2fa8dd04a64dd
itwillbeusedalot <86040413+itwillbeusedalot@users.noreply.github.com>
2024-12-10 23:47:42 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,4 @@
 uname -a
-apt update
-sudo apt update
 cat /etc/*ease
 
 dpkg-query -W --showformat='${Installed-Size}\t${Package}\n' | awk '{printf "%10.2f MB \t%s\n", $1/1024, $2}' | sort -n > package_list_size_in_MB.txt
