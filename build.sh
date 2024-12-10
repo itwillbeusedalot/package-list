357840c38fe4989774c9a27e7ada1abef57f8e67
itwillbeusedalot <86040413+itwillbeusedalot@users.noreply.github.com>
2024-12-11 00:10:49 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -5,98 +5,22 @@ dpkg-query -W --showformat='${Installed-Size}\t${Package}\n' | awk '{printf "%10
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n > package_list_size_in_bytes.txt
 
-apt purge -y --allow-remove-essential libaudit-common
-apt purge -y --allow-remove-essential libsemanage-common
-apt purge -y --allow-remove-essential ubuntu-keyring
-apt purge -y --allow-remove-essential libdebconfclient0
-apt purge -y --allow-remove-essential libnpth0t64
-apt purge -y --allow-remove-essential hostname
-apt purge -y --allow-remove-essential logsave
-apt purge -y --allow-remove-essential libcom-err2
-apt purge -y --allow-remove-essential libattr1
-apt purge -y --allow-remove-essential libcap-ng0
-apt purge -y --allow-remove-essential sensible-utils
-apt purge -y --allow-remove-essential libss2
-apt purge -y --allow-remove-essential libffi8
-apt purge -y --allow-remove-essential libacl1
-apt purge -y --allow-remove-essential libmd0
-apt purge -y --allow-remove-essential libuuid1
-apt purge -y --allow-remove-essential libxxhash0
-apt purge -y --allow-remove-essential libbz2-1.0
-apt purge -y --allow-remove-essential libcap2
-apt purge -y --allow-remove-essential libassuan0
-apt purge -y --allow-remove-essential sysvinit-utils
-apt purge -y --allow-remove-essential gcc-14-base
-apt purge -y --allow-remove-essential libtasn1-6
-apt purge -y --allow-remove-essential init-system-helpers
-apt purge -y --allow-remove-essential libseccomp2
-apt purge -y --allow-remove-essential libaudit1
-apt purge -y --allow-remove-essential liblz4-1
-apt purge -y --allow-remove-essential zlib1g
-apt purge -y --allow-remove-essential libsmartcols1
-apt purge -y --allow-remove-essential libgpg-error0
-apt purge -y --allow-remove-essential dash
-apt purge -y --allow-remove-essential libgcc-s1
-apt purge -y --allow-remove-essential libselinux1
-apt purge -y --allow-remove-essential libpam0g
-apt purge -y --allow-remove-essential debianutils
-apt purge -y --allow-remove-essential libidn2-0
-apt purge -y --allow-remove-essential libcrypt1
-apt purge -y --allow-remove-essential libproc2-0
-apt purge -y --allow-remove-essential gzip
-apt purge -y --allow-remove-essential base-passwd
-apt purge -y --allow-remove-essential libpam-modules-bin
-apt purge -y --allow-remove-essential mawk
-apt purge -y --allow-remove-essential libpam-runtime
-apt purge -y --allow-remove-essential bsdutils
-apt purge -y --allow-remove-essential libsemanage2
-apt purge -y --allow-remove-essential libblkid1
-apt purge -y --allow-remove-essential libudev1
-apt purge -y --allow-remove-essential gpgv
-apt purge -y --allow-remove-essential libhogweed6t64
-apt purge -y --allow-remove-essential sed
-apt purge -y --allow-remove-essential grep
-apt purge -y --allow-remove-essential mount
-apt purge -y --allow-remove-essential libmount1
-apt purge -y --allow-remove-essential liblzma5
-apt purge -y --allow-remove-essential ncurses-base
-apt purge -y --allow-remove-essential libncursesw6
-apt purge -y --allow-remove-essential libnettle8t64
-apt purge -y --allow-remove-essential base-files
-apt purge -y --allow-remove-essential diffutils
-apt purge -y --allow-remove-essential debconf
-apt purge -y --allow-remove-essential libext2fs2t64
-apt purge -y --allow-remove-essential libgmp10
-apt purge -y --allow-remove-essential findutils
-apt purge -y --allow-remove-essential libtinfo6
-apt purge -y --allow-remove-essential libpcre2-8-0
-apt purge -y --allow-remove-essential ncurses-bin
-apt purge -y --allow-remove-essential tar
-apt purge -y --allow-remove-essential libsepol2
-apt purge -y --allow-remove-essential libzstd1
-apt purge -y --allow-remove-essential login
-apt purge -y --allow-remove-essential libsystemd0
-apt purge -y --allow-remove-essential libpam-modules
-apt purge -y --allow-remove-essential libgcrypt20
-apt purge -y --allow-remove-essential e2fsprogs
-apt purge -y --allow-remove-essential libp11-kit0
-apt purge -y --allow-remove-essential libunistring5
-apt purge -y --allow-remove-essential libdb5.3t64
-apt purge -y --allow-remove-essential procps
-apt purge -y --allow-remove-essential bash
-apt purge -y --allow-remove-essential libc-bin
-apt purge -y --allow-remove-essential libgnutls30t64
-apt purge -y --allow-remove-essential passwd
-apt purge -y --allow-remove-essential libstdc++6
-apt purge -y --allow-remove-essential libapt-pkg6.0t64
-apt purge -y --allow-remove-essential util-linux
-apt purge -y --allow-remove-essential apt
-apt purge -y --allow-remove-essential dpkg
-apt purge -y --allow-remove-essential libssl3t64
-apt purge -y --allow-remove-essential coreutils
-apt purge -y --allow-remove-essential perl-base
-apt purge -y --allow-remove-essential libc6
+# Get all installed packages
+PACKAGES_TO_KEEP="apparmor apt dbus e2fsprogs netbase sudo systemd systemd-resolved systemd-sysv udev"
 
+# Get all installed packages
+INSTALLED_PACKAGES=$(dpkg-query -W -f='${Package}\n')
+
+# Remove packages not in the list
+for package in $INSTALLED_PACKAGES; do
+  if ! echo "$PACKAGES_TO_KEEP" | grep -q "$package"; then
+    apt purge -y "$package"
+  fi
+done
+
+# Remove any remaining unnecessary packages
+apt autoclean -y
+apt clean
 
 dpkg-query -Wf '${Installed-Size}\t${Package}\n' | sort -n > purge_list.txt
 
